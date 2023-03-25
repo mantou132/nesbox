94f0b4b86a1e1a28f617556a5734d2f4d66c264a
mantou132 <709922234@qq.com>
2023-03-26 01:31:50 +0800

    Fixed @mantou/nes dep

--- scripts/nes-pkg.mjs
+++ scripts/nes-pkg.mjs
@@ -1,14 +1,43 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import { exec } from 'node:child_process';
 
 import pkg from '../packages/nes-pkg/package.json' assert { type: 'json' };
 
-const pathname = path.resolve(process.cwd(), 'packages/nes-pkg', pkg.module);
-const content = await fs.readFile(pathname, {
-  encoding: 'utf8',
-});
+function executeShellCommand(command, cwd = process.cwd()) {
+  return new Promise((resolve, reject) => {
+    exec(
+      command,
+      {
+        cwd,
+        env: {
+          PATH: process.env.PATH,
+        },
+      },
+      (error, stdout) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        process.stdout.write(stdout);
+        resolve(stdout);
+      },
+    );
+  });
+}
 
-await fs.writeFile(
-  pathname,
-  content.replace(/new Function\(.*\)/g, 'new Function("console.warn(`new Function be replaced`)")'),
-);
+await executeShellCommand('yarn build:nes --debug');
+
+const pathname = path.resolve(process.cwd(), 'packages/nes-pkg', pkg.module);
+
+const content = (
+  await fs.readFile(pathname, {
+    encoding: 'utf8',
+  })
+).replace(/new Function\(.*\)/g, 'new Function("console.warn(`new Function be replaced`)")');
+
+await executeShellCommand('yarn build:nes');
+
+await fs.writeFile(pathname, content);
+
+await executeShellCommand('npm publish --access=public', path.resolve(process.cwd(), 'packages/nes-pkg'));
